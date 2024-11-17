d1add79c62ab082ac251bd4bf5086559da5671d2
gravitygo <chyle.andrei.lee@gmail.com>
2024-11-17 14:43:49 +0800

    Handle async better
    UI fix and handled the async better since previously the async function
    implementation didn't work as expected. Fixed the bug of can press
    multiple times without waiting for the previous transaction to load
    leading to mismatch in the amount of clicks to the amount reflected in
    the balance

--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -3,38 +3,59 @@ import { dbank_backend } from 'declarations/dbank_backend';
 
 function App() {
   const [balance, setBalance] = useState(0);
+  const [load, setLoad] = useState(false);
 
-  useEffect(() => getBalance(), []);
+  useEffect(() => {
+    const fetchBalance = async () => {
+      setLoad(true)
+      const balance = await dbank_backend.checkBalance();
+      setBalance(balance);
+      setLoad(false)
+    }
+    fetchBalance();
+  }, []);
 
-  function getBalance() {
-    dbank_backend.checkBalance().then((accountBalance) => {
-      setBalance(accountBalance)
-    })
+  async function getBalance() {
+    try {
+      setLoad(true)
+      const accountBalance = await dbank_backend.checkBalance();
+      setBalance(accountBalance);
+    } catch (error) {
+      console.error("failed to get the balance:", error);
+    } finally {
+      setLoad(false)
+    }
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    const topUp = event.target.elements.topUp.value;
-    const withdraw = event.target.elements.withdraw.value;
+    if (load) return false;
+    const topUp = parseFloat(event.target.elements.topUp.value || 0);
+    const withdraw = parseFloat(event.target.elements.withdraw.value || 0);
 
+    try {
+      setLoad(true)
+      await dbank_backend.topUp(topUp);
+      await dbank_backend.withdraw(withdraw);
+      await getBalance();
+    } catch (error) {
+      console.error("Failed transaction:", error)
+    } finally {
+      setLoad(false)
+    }
 
-    dbank_backend.topUp(parseFloat(topUp) || 0);
-    dbank_backend.withdraw(parseFloat(withdraw) || 0);
-
-    getBalance();
     return false;
   }
 
   return (
     <main className="p-3 rounded flex flex-col items-center justify-center bg-blue-500">
-      <p>{(Math.round(balance * 100) / 100).toFixed(2)}</p>
-      <form className="flex flex-col" action="#" onSubmit={handleSubmit}>
-        <label htmlFor="topUp">Amount to Top Up</label>
+      <h3 className="text-lg font-medium text-slate-50">${(Math.round(balance * 100) / 100).toFixed(2)}</h3>
+      <form className="gap-2 flex flex-col" action="#" onSubmit={handleSubmit}>
+        <label htmlFor="topUp" className='font-mono text-slate-300'>Amount to Top Up</label>
         <input id="topUp" alt="top up" type="number" />
-        <br />
-        <label htmlFor="withdraw">Amount to Withdraw</label>
+        <label htmlFor="withdraw" className='font-mono text-slate-300'>Amount to Withdraw</label>
         <input id="withdraw" alt="withdraw" type="number" />
-        <button type="submit">Click Me!</button>
+        <button type="submit" disabled={load} className='rounded text-white px-4 py-2 disabled:bg-gray-600 disabled:hover:bg-gray-600 bg-slate-500 hover:bg-slate-600'>Click Me!{load}</button>
       </form>
     </main>
   );
